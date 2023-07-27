2599dbf24ee74e101e37cfa5bbd491e45c950e7f
chrisxhhh <qx428@nyu.edu>
2023-07-26 22:16:16 -0400

    set up setting storage and simplified popup

--- popup/popup.js
+++ popup/popup.js
@@ -9,14 +9,7 @@ import {
 } from "../handlers/summaryHandler.js";
 import { getSummaryRequest } from "../api/openAI.js";
 
-function setSwitchDisableStatus(element, status) {
-    if (!status && element.nodeName === 'INPUT') {
-        element.checked = false;
-        currentSwitchStatus[element.id] = false;
-        updateSwitchStatus(element);
-    }
-    element.disabled = !status;
-}
+
 
 const elements = [
     // switch
@@ -30,172 +23,233 @@ const elements = [
     document.getElementById("targetFont"),
 ];
 
-let currentSwitchStatus = {
-    "turn-on-toggle": false,
-    'translate-toggle': false,
-    'simple-version-toggle': false,
-    'customize-font-toggle': false,
-    'summary-toggle': false,
-    'targetLanguage': 'cn',
-    'targetFont': 'Arial',
+const toggleNameToStorageName = {
+    "turn-on-toggle": "main-toggle",
+    "translate-toggle": "translate",
+    "simple-version-toggle": "simple",
+    "customize-font-toggle": "customize",
+    "summary-toggle": "summary",
+    "targetLanguage": "targetLanguage",
+    "targetFont": "targetFont"
 }
 
 window.onload = () => {
-    chrome.storage.sync.get(['extensionSwitchs'], function (result) {
-        console.log('onload: Value currently is ' + result.extensionSwitchs);
-        console.log(result)
-        if (result.extensionSwitchs) {
-            currentSwitchStatus = result.extensionSwitchs;
-            document.getElementById("turn-on-toggle").checked = currentSwitchStatus["turn-on-toggle"];
-            if (currentSwitchStatus["turn-on-toggle"]) {
-                elements.forEach(e => setSwitchDisableStatus(e, true));
+    chrome.storage.sync.get(['settings']).then((result) => {
+        console.log('onload:' + JSON.stringify(result.settings));
+        let currSettings;
+        if (result.settings) {
+            currSettings = result.settings;
+        }
+        document.getElementById("turn-on-toggle").checked = currSettings["main-toggle"]["toggle"];
+        if (currSettings["main-toggle"]["toggle"]) {
+            elements.forEach(e => setSwitchDisableStatus(e, true));
+        }
+        elements.forEach(e => {
+            if (e.nodeName === 'INPUT') {
+                e.checked = currSettings[toggleNameToStorageName[e.id]]['toggle'];
             }
-            elements.forEach(e => {
-                if (e.nodeName === 'INPUT') {
-                    e.checked = currentSwitchStatus[e.id];
-                }
-                else if (e.nodeName === 'SELECT') {
-                    e.value = currentSwitchStatus[e.id];
-                }
-            });
-
-        }
-
-        else {
-            chrome.storage.sync.set({ extensionSwitchs: currentSwitchStatus }, function () {
-                console.log('first time onload: Value is set to ' + currentSwitchStatus);
-            });
-        }
+            else if (e.id === 'targetLanguage') {
+                e.value = currSettings["translate"]['language'];
+            }
+            else if (e.id === 'targetFont') {
+                e.value = currSettings["customize"]['font'];
+            }
+        });
     });
 }
 
 function updateSwitchStatus(element) {
-    console.log(element.checked);
-    element.nodeName === "INPUT" ?
-        currentSwitchStatus[element.id] = element.checked :
-        currentSwitchStatus[element.id] = element.value;
-    console.log(`updateSwitchStatus: ${currentSwitchStatus}`)
-    chrome.storage.sync.set({ extensionSwitchs: currentSwitchStatus }, function () {
-        console.log(currentSwitchStatus);
+    chrome.storage.sync.get(['settings']).then((result) => {
+        console.log(result)
+        let currSettings = result.settings;
+        if (element.nodeName === "INPUT") {
+            console.log(element.id + " is " + element.checked);
+            console.log(currSettings[toggleNameToStorageName[element.id]])
+            currSettings[toggleNameToStorageName[element.id]]['toggle'] = element.checked
+        }
+        else if (element.id === 'targetLanguage') {
+            currSettings["translate"]['language'] = element.value;
+        }else {
+            currSettings["customize"]['font'] = element.value;
+        }
+        console.log(currSettings);
+        return currSettings;
+    }).then((currSettings) => {
+        chrome.storage.sync.set({ settings: currSettings });
     });
+}
+
+function setSwitchDisableStatus(element, status)  {
+    if (!status && element.nodeName === 'INPUT' && element.checked) {
+        element.checked = false;
+        updateSwitchStatus(element);
+    }
+    element.disabled = !status;
+}
+
+const defaultSettings = {
+    "main-toggle": {
+      "toggle": false,
+    },
+    "translate": {
+      "toggle": false,
+      "language": "cn",
+      "color": "grey",
+      "style": "none"
+    },
+    "customize": {
+      "toggle": false,
+      "font": "Arial"
+    },
+    "simple": {
+      "toggle": false,
+      "age": 1
+    },
+    "summary": {
+      "toggle": false,
+      "short_length": 100,
+      "long_length": 200
+    }
+  }
+function setBackToDefault() {
+    elements.forEach(e => {
+        e.disabled = true;
+        if (e.nodeName === 'INPUT'){
+            e.checked = false;
+        } 
+        else if (e.id === 'targetLanguage') {
+            e.value = defaultSettings["translate"]['language'];
+        }else {
+            e.value = defaultSettings["customize"]['font'];
+        }
+    });
+    chrome.storage.sync.set({ settings: defaultSettings })
 }
 
 
 function enableExtension() {
-    updateSwitchStatus(this);
     if (this.checked) {
         console.log("Checked the turn on option!");
-        elements.forEach(e => setSwitchDisableStatus(e, true));
+        updateSwitchStatus(this);
+        elements.forEach(e => e.disabled = false)
     } else {
         console.log("Unchecked the turn on option!");
-        elements.forEach(e => setSwitchDisableStatus(e, false));
+        setBackToDefault();
+        
     }
 
 }
-document.getElementById("turn-on-toggle").addEventListener("click", enableExtension);
+document.getElementById("turn-on-toggle").addEventListener("change", enableExtension);
 
-
-function onClickTranslate() {
-    updateSwitchStatus(this);
-    if (this.checked) {
-        console.log("Checked translate");
-        let targetLanguage = document.getElementById('targetLanguage').value;
-        console.log(`targetLanguage: ${targetLanguage}`);
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: translateArticleBody,
-                args: [targetLanguage]
-            })
-        });
-    } else {
-        console.log('Unchecked the translate option!');
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: removeTranslation
-            })
-        });
+elements.forEach(e => {
+    if (e.nodeName === 'INPUT') {
+        e.addEventListener("change", ()=> updateSwitchStatus(e));
+    } else{
+        e.addEventListener("change", ()=> updateSwitchStatus(e));
     }
-}
-document
-  .getElementById("translate-toggle")
-  .addEventListener("change", onClickTranslate);
-
-function onClickSimpleEnglish() {
-    updateSwitchStatus(this);
-    if (this.checked) {
-        console.log("clicked the simple version option");
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: simplifyArticleBody
-            })
-        });
-    } else {
-        console.log("unchecked the simple version option");
-    }
-}
-document
-  .getElementById("simple-version-toggle")
-  .addEventListener("change", onClickSimpleEnglish);
-
-function onChangeCustomizeFont() {
-    updateSwitchStatus(this);
-    if (this.checked) {
-        console.log('Checkbox is checked!');
-    } else {
-        console.log('Checkbox is unchecked!');
-    }
-}
-document
-  .getElementById("customize-font-toggle")
-  .addEventListener("change", onChangeCustomizeFont);
+});
 
 
-function onChangeSummary() {
-    updateSwitchStatus(this);
-    if (this.checked) {
-        console.log("Clicked summary");
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.scripting
-              .executeScript({
-                target: { tabId: tabs[0].id },
-                func: getSummaryRequest,
-                args: [100],
-                // func: function() {
-                //     chrome.tabs.executeScript({target: {tabId: tabs[0].id}, func: generateSummary})
-                // },
-              })
-              .then((res) => {
-                console.log(res[0].result);
-                chrome.scripting.executeScript({
-                  target: { tabId: tabs[0].id },
-                  func: generateSummary,
-                  args: [res[0].result],
-                });
-              });
-            });
-    } else {
-        console.log('Unchecked the summary option!');
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: removeSummarySection
-            })
-        });
-    }
-}
-document.getElementById("summary-toggle").addEventListener("change", onChangeSummary);
 
-function onChangeTargetLanguage() {
-    updateSwitchStatus(this);
-    console.log(this.value);
-}
-document.getElementById("targetLanguage").addEventListener("change", onChangeTargetLanguage);
+// function onClickTranslate() {
+//     updateSwitchStatus(this);
+//     if (this.checked) {
+//         console.log("Checked translate");
+//         let targetLanguage = document.getElementById('targetLanguage').value;
+//         console.log(`targetLanguage: ${targetLanguage}`);
+//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//             chrome.scripting.executeScript({
+//                 target: { tabId: tabs[0].id },
+//                 func: translateArticleBody,
+//                 args: [targetLanguage]
+//             })
+//         });
+//     } else {
+//         console.log('Unchecked the translate option!');
+//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//             chrome.scripting.executeScript({
+//                 target: { tabId: tabs[0].id },
+//                 func: removeTranslation
+//             })
+//         });
+//     }
+// }
+// document
+//   .getElementById("translate-toggle")
+//   .addEventListener("change", onClickTranslate);
 
-function onChangeTargetFont() {
-    updateSwitchStatus(this);
-    console.log(this.value);
-}
-document.getElementById("targetFont").addEventListener("change", onChangeTargetFont);
+// function onClickSimpleEnglish() {
+//     updateSwitchStatus(this);
+//     if (this.checked) {
+//         console.log("clicked the simple version option");
+//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//             chrome.scripting.executeScript({
+//                 target: { tabId: tabs[0].id },
+//                 func: simplifyArticleBody
+//             })
+//         });
+//     } else {
+//         console.log("unchecked the simple version option");
+//     }
+// }
+// document
+//   .getElementById("simple-version-toggle")
+//   .addEventListener("change", onClickSimpleEnglish);
+
+// function onChangeCustomizeFont() {
+//     updateSwitchStatus(this);
+//     if (this.checked) {
+//         console.log('Checkbox is checked!');
+//     } else {
+//         console.log('Checkbox is unchecked!');
+//     }
+// }
+// document
+//   .getElementById("customize-font-toggle")
+//   .addEventListener("change", onChangeCustomizeFont);
+
+
+// function onChangeSummary() {
+//     updateSwitchStatus(this);
+//     if (this.checked) {
+//         console.log("Clicked summary");
+//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//             chrome.scripting
+//               .executeScript({
+//                 target: { tabId: tabs[0].id },
+//                 func: getSummaryRequest,
+//                 args: [100],
+//                 // func: function() {
+//                 //     chrome.tabs.executeScript({target: {tabId: tabs[0].id}, func: generateSummary})
+//                 // },
+//               })
+//               .then((res) => {
+//                 console.log(res[0].result);
+//                 chrome.scripting.executeScript({
+//                   target: { tabId: tabs[0].id },
+//                   func: generateSummary,
+//                   args: [res[0].result],
+//                 });
+//               });
+//             });
+//     } else {
+//         console.log('Unchecked the summary option!');
+//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//             chrome.scripting.executeScript({
+//                 target: { tabId: tabs[0].id },
+//                 func: removeSummarySection
+//             })
+//         });
+//     }
+// }
+// document.getElementById("summary-toggle").addEventListener("change", onChangeSummary);
+
+// function onChangeTargetLanguage() {
+//     updateSwitchStatus(this);
+// }
+// document.getElementById("targetLanguage").addEventListener("change", onChangeTargetLanguage);
+
+// function onChangeTargetFont() {
+//     updateSwitchStatus(this);
+// }
+// document.getElementById("targetFont").addEventListener("change", onChangeTargetFont);
