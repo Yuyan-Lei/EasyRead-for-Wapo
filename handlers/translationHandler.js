8930cc7520a04fdfac0020ed51e225a25f264b40
samuelshix <sam.shi@washpost.com>
2023-07-27 17:47:47 -0400

    g translate

--- handlers/translationHandler.js
+++ handlers/translationHandler.js
@@ -1,41 +1,85 @@
-import { OPEN_AI_KEY } from "../apiKey.js";
+import { OPEN_AI_KEY, GOOGLE_TRANSLATE_API_KEY } from "../apiKey.js";
+
+const LANGUAGE_CODE_MAPPINGS = {
+  Spanish: "es",
+  "Simplified Chinese": "zh-CN",
+};
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log("Listener called --  translation");
-    if (message.action === "getTranslation") {
-      getTranslationRequest(message.targetLanguage, message.text).then((result) => {
-            sendResponse({ result: result });
-        });
-        return true;
-    }
+  console.log("Listener called --  translation");
+  if (message.action === "getTranslation") {
+    getTranslationRequest(message.targetLanguage, message.text).then(
+      (result) => {
+        sendResponse({ result: result });
+      }
+    );
+    return true;
+  }
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log("Listener called -- Google translation");
+  if (message.action === "getGoogleTranslation") {
+    sendGTranslateRequest(message.targetLanguage, message.text).then(
+      (result) => {
+        sendResponse({ result: result });
+      }
+    );
+    return true;
+  }
 });
 
 async function sendRequest(requestData) {
-    const apiKey = OPEN_AI_KEY;
-    const data = await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify(requestData),
-    }).then((response) => response.json());
-    return data.choices[0].message.content;
+  const apiKey = OPEN_AI_KEY;
+  const data = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify(requestData),
+  }).then((response) => response.json());
+  return data.choices[0].message.content;
 }
-  
+
 async function getTranslationRequest(targetLanguage, originalText) {
-    console.log(originalText);
-    console.log("run API now -- translation");
-    const requestData = {
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: `You are a translation assistant. I will send a English text each time, and you need to respond me the translated text in ${targetLanguage}. Do not add any extra punctuations if there is no such punctuation in the original text. Do not respond any thing else than translated text.` },
-        {
-          role: "user",
-          content: `${originalText}`,
-        },
-      ],
-      temperature: 1,
-    };
-  
-    return sendRequest(requestData);
+  console.log(originalText);
+  console.log("run API now -- translation");
+  const requestData = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: `You are a translation assistant. I will send a English text each time, and you need to respond me the translated text in ${targetLanguage}. Do not add any extra punctuations if there is no such punctuation in the original text. Do not respond any thing else than translated text.`,
+      },
+      {
+        role: "user",
+        content: `${originalText}`,
+      },
+    ],
+    temperature: 1,
+  };
+
+  return sendRequest(requestData);
+}
+
+async function sendGTranslateRequest(targetLanguage, originalText) {
+  console.log("run google translate API now");
+  const languageCode = LANGUAGE_CODE_MAPPINGS[targetLanguage];
+  const data = await fetch(
+    "https://translation.googleapis.com/language/translate/v2" +
+      new URLSearchParams({
+        q: originalText,
+        target: languageCode,
+        format: text,
+        source: "en",
+        model: "nmt",
+        key: GOOGLE_TRANSLATE_API_KEY,
+      }),
+    {
+      method: "POST",
+    }
+  );
+  console.log("G translate response data: " + data);
+  return data.data.translations[0];
 }
--- scripts/translation.js
+++ scripts/translation.js
@@ -1,175 +1,190 @@
 (async () => {
-    chrome.storage.sync.get(['settings']).then(async(result) => {
-        const mainSettings = result.settings['main-toggle']['toggle'];
-        const on = result.settings['translate']['toggle'];
-        const style = result.settings['translate']['style'];
-        const color = result.settings['translate']['color'];
-        const targetLanguage = result.settings['translate']['language'];
-        if (mainSettings && on) {
-            function buildNode(isATag) {
-                let translationParagraph;
-                translationParagraph = document.createElement('p');
-                translationParagraph.innerHTML = 'Translating...';
-                translationParagraph.id = 'translation-section';
+  chrome.storage.sync.get(["settings"]).then(async (result) => {
+    const mainSettings = result.settings["main-toggle"]["toggle"];
+    const on = result.settings["translate"]["toggle"];
+    const style = result.settings["translate"]["style"];
+    const color = result.settings["translate"]["color"];
+    const targetLanguage = result.settings["translate"]["language"];
+    const translateService = result.settings["translate"]["service"];
 
-                if (!isATag) {
-                    switch (style) {
-                    case 'none':
-                        break;
-                    case 'italic':
-                        translationParagraph.style.fontStyle = 'italic';          
-                        break;
-                    case 'bold':
-                        translationParagraph.style.fontWeight = 'bold';          
-                        break;
-                    case 'underlined':
-                        translationParagraph.style.textDecorationLine = 'underline'; 
-                        translationParagraph.style.textDecorationColor = '#018abe';          
-                        break;
-                    case 'dashed-underlined':
-                        translationParagraph.style.textDecorationLine = 'underline';          
-                        translationParagraph.style.textDecorationColor = '#018abe'; 
-                        translationParagraph.style.textDecorationStyle = 'dashed';          
-                        break;
-                    case 'highlighted':
-                        translationParagraph.style.backgroundColor = 'yellow';         
-                        break;
-                    default:
-                        break;
-                    }
-                }
+    if (mainSettings && on) {
+      function buildNode(isATag) {
+        let translationParagraph;
+        translationParagraph = document.createElement("p");
+        translationParagraph.innerHTML = "Translating...";
+        translationParagraph.id = "translation-section";
 
-                // Display color options:
-                switch (color) {
-                    case 'none':      
-                    break;
-                    case 'grey':
-                    translationParagraph.style.color = 'grey';        
-                    break;
-                    case 'blue':
-                    translationParagraph.style.color = '#018abe';        
-                    break;
-                    case 'orange':
-                    translationParagraph.style.color = 'orange';        
-                    break;
-                    case 'red':
-                    translationParagraph.style.color = 'red';        
-                    break;
-                    case 'green':
-                    translationParagraph.style.color = 'green';        
-                    break;
-                    default:
-                    break;
-                }
-
-                return translationParagraph;
-            }
-            async function processNewNode(node, position) {
-                // build node
-                let newNode = buildNode(false);
-                node.insertAdjacentElement(position, newNode);
-                // translate text
-                const response = await chrome.runtime.sendMessage({
-                    action: 'getTranslation',
-                    text: node.innerText,
-                    targetLanguage: targetLanguage,
-                })
-                const text = response.result;
-                newNode.innerText = text;
-            }
-
-            async function processDropCapLetter(node) {
-                let translation = '';
-                let paragraphText = node.innerHTML;
-
-                // build a new node
-                let newNode = buildNode(false);
-                node.insertAdjacentElement('beforeend', newNode);
-
-                // remove bold tags
-                const regexToRemoveBoldTag = /<b>\ <\/b>/g;
-                paragraphText = paragraphText.replace(regexToRemoveBoldTag, "");
-            
-                const regex = /(<a[^>]*>.*?<\/a>)|[^<>]+/g;
-                let match;
-                while ((match = regex.exec(paragraphText)) !== null) {
-                    if (match[1]) {
-                        const regexATag = /(<a[^>]*>)(.*?)(<\/a>)/g;
-                        let openingTag, textBeforeTrans, closingTag;
-                        match[1].replace(regexATag, (match, before, text, after) => {
-                            // get translation from API
-                            openingTag = before;
-                            textBeforeTrans = text;
-                            closingTag = after;
-                        });
-                        const textResponse = await chrome.runtime.sendMessage({
-                            action: 'getTranslation',
-                            text: textBeforeTrans,
-                            targetLanguage: targetLanguage,
-                        })
-                        const translatedText = textResponse.result;
-                        const result = ` ${openingTag}${translatedText}${closingTag} `;
-                        translation += result;
-                    } else if (match[0]) {
-                        // get translation from API
-                        const textResponse = await chrome.runtime.sendMessage({
-                            action: 'getTranslation',
-                            text: match[0],
-                            targetLanguage: targetLanguage,
-                        })
-                        const translatedText = textResponse.result;
-                        translation += translatedText;
-                    }
-                }
-                newNode.innerHTML = translation;
-            }
-
-            async function processInterstitialLink(node) {
-                // build a new node
-                const newNode= buildNode(true);
-                node.insertAdjacentElement("afterend", newNode);
-
-                // translation
-                const link = node.href;
-                const text = node.innerText;
-                // get translation from API
-                const textResponse = await chrome.runtime.sendMessage({
-                    action: 'getTranslation',
-                    text: text,
-                    targetLanguage: targetLanguage,
-                })
-                const translatedText = textResponse.result;
-                newNode.innerHTML = `<a href="${link}">${translatedText}</a>`;
-            }
-
-            async function traverseBodyNodes(node) {
-                if (node.nodeType === Node.ELEMENT_NODE) {
-                    if (node.matches('span[data-qa="headline-text"]')) {
-                        await processNewNode(node, 'afterend');
-                    } else if (node.matches('span[data-qa="headline-opinion-text"]')) {
-                        await processNewNode(node, 'afterend');
-                    } else if (node.matches('h2[data-qa="subheadline"]')) {
-                        await processNewNode(node, 'beforeend');
-                    } else if (node.matches ('figcaption')) {
-                        await processNewNode(node, 'beforeend');
-                    } else if (node.matches('p[data-testid="drop-cap-letter"]')) {
-                        await processDropCapLetter(node);
-                    } else if (node.matches('a[data-qa="interstitial-link"]')) {
-                        await processInterstitialLink(node);
-                    }
-                }
-                for (const childNode of node.childNodes) {
-                    traverseBodyNodes(childNode);
-                }
-            }
-
-            await traverseBodyNodes(document.body);
-        } else {
-            // remove translation
-            const translationParagraphs = document.querySelectorAll('#translation-section');
-            translationParagraphs.forEach((paragraph) => {
-            paragraph.remove();
-            });
+        if (!isATag) {
+          switch (style) {
+            case "none":
+              break;
+            case "italic":
+              translationParagraph.style.fontStyle = "italic";
+              break;
+            case "bold":
+              translationParagraph.style.fontWeight = "bold";
+              break;
+            case "underlined":
+              translationParagraph.style.textDecorationLine = "underline";
+              translationParagraph.style.textDecorationColor = "#018abe";
+              break;
+            case "dashed-underlined":
+              translationParagraph.style.textDecorationLine = "underline";
+              translationParagraph.style.textDecorationColor = "#018abe";
+              translationParagraph.style.textDecorationStyle = "dashed";
+              break;
+            case "highlighted":
+              translationParagraph.style.backgroundColor = "yellow";
+              break;
+            default:
+              break;
+          }
         }
-    });
+
+        // Display color options:
+        switch (color) {
+          case "none":
+            break;
+          case "grey":
+            translationParagraph.style.color = "grey";
+            break;
+          case "blue":
+            translationParagraph.style.color = "#018abe";
+            break;
+          case "orange":
+            translationParagraph.style.color = "orange";
+            break;
+          case "red":
+            translationParagraph.style.color = "red";
+            break;
+          case "green":
+            translationParagraph.style.color = "green";
+            break;
+          default:
+            break;
+        }
+
+        return translationParagraph;
+      }
+      async function processNewNode(node, position) {
+        // build node
+        let newNode = buildNode(false);
+        node.insertAdjacentElement(position, newNode);
+        // translate text
+        let response;
+        if (translateService === "openai") {
+          response = await chrome.runtime.sendMessage({
+            action: "getTranslation",
+            text: node.innerText,
+            targetLanguage: targetLanguage,
+          });
+        } else if (translateService === "google") {
+          response = await chrome.runtime.sendMessage({
+            action: "getGoogleTranslation",
+            text: node.innerText,
+            targetLanguage: targetLanguage,
+          });
+        } else {
+          console.log("Invalid translate service");
+        }
+        const text = response.result;
+        newNode.innerText = text;
+      }
+
+      async function processDropCapLetter(node) {
+        let translation = "";
+        let paragraphText = node.innerHTML;
+
+        // build a new node
+        let newNode = buildNode(false);
+        node.insertAdjacentElement("beforeend", newNode);
+
+        // remove bold tags
+        const regexToRemoveBoldTag = /<b>\ <\/b>/g;
+        paragraphText = paragraphText.replace(regexToRemoveBoldTag, "");
+
+        const regex = /(<a[^>]*>.*?<\/a>)|[^<>]+/g;
+        let match;
+        while ((match = regex.exec(paragraphText)) !== null) {
+          if (match[1]) {
+            const regexATag = /(<a[^>]*>)(.*?)(<\/a>)/g;
+            let openingTag, textBeforeTrans, closingTag;
+            match[1].replace(regexATag, (match, before, text, after) => {
+              // get translation from API
+              openingTag = before;
+              textBeforeTrans = text;
+              closingTag = after;
+            });
+            const textResponse = await chrome.runtime.sendMessage({
+              action: "getTranslation",
+              text: textBeforeTrans,
+              targetLanguage: targetLanguage,
+            });
+            const translatedText = textResponse.result;
+            const result = ` ${openingTag}${translatedText}${closingTag} `;
+            translation += result;
+          } else if (match[0]) {
+            // get translation from API
+            const textResponse = await chrome.runtime.sendMessage({
+              action: "getTranslation",
+              text: match[0],
+              targetLanguage: targetLanguage,
+            });
+            const translatedText = textResponse.result;
+            translation += translatedText;
+          }
+        }
+        newNode.innerHTML = translation;
+      }
+
+      async function processInterstitialLink(node) {
+        // build a new node
+        const newNode = buildNode(true);
+        node.insertAdjacentElement("afterend", newNode);
+
+        // translation
+        const link = node.href;
+        const text = node.innerText;
+        // get translation from API
+        const textResponse = await chrome.runtime.sendMessage({
+          action: "getTranslation",
+          text: text,
+          targetLanguage: targetLanguage,
+        });
+        const translatedText = textResponse.result;
+        newNode.innerHTML = `<a href="${link}">${translatedText}</a>`;
+      }
+
+      async function traverseBodyNodes(node) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          if (node.matches('span[data-qa="headline-text"]')) {
+            await processNewNode(node, "afterend");
+          } else if (node.matches('span[data-qa="headline-opinion-text"]')) {
+            await processNewNode(node, "afterend");
+          } else if (node.matches('h2[data-qa="subheadline"]')) {
+            await processNewNode(node, "beforeend");
+          } else if (node.matches("figcaption")) {
+            await processNewNode(node, "beforeend");
+          } else if (node.matches('p[data-testid="drop-cap-letter"]')) {
+            await processDropCapLetter(node);
+          } else if (node.matches('a[data-qa="interstitial-link"]')) {
+            await processInterstitialLink(node);
+          }
+        }
+        for (const childNode of node.childNodes) {
+          traverseBodyNodes(childNode);
+        }
+      }
+
+      await traverseBodyNodes(document.body);
+    } else {
+      // remove translation
+      const translationParagraphs = document.querySelectorAll(
+        "#translation-section"
+      );
+      translationParagraphs.forEach((paragraph) => {
+        paragraph.remove();
+      });
+    }
+  });
 })();
